265cd6f4775de882cfc82251e311ef247e6ebd4d
Seulgi Kim <dev@seulgi.kim>
2015-02-09 22:45:34 +0900

    Support .md

--- app.py
+++ app.py
@@ -18,6 +18,20 @@ def convert_to_html(md):
     return markdown.markdown(md, extensions=['attr_list'])
 
 
+class MarkdownHandler(tornado.web.RequestHandler):
+    def initialize(self, target):
+        self.target = target
+
+    @tornado.web.asynchronous
+    def get(self, path):
+        get_md_async(self.target, path, self._on_download)
+
+    def _on_download(self, response):
+        self.write(response.body.decode("utf-8"))
+        self.set_header('Content-Type', 'text/plain; charset="utf-8"')
+        self.finish()
+
+
 class HTMLHandler(tornado.web.RequestHandler):
     def initialize(self, target):
         self.target = target
@@ -35,6 +49,7 @@ if __name__ == '__main__':
     assert (len(sys.argv) == 2)
     args = dict(target=sys.argv[1])
     Application = tornado.web.Application
-    application = Application([(r"/(.*)\.html", HTMLHandler, args)])
+    application = Application([(r"/(.*)\.html", HTMLHandler, args),
+                               (r"/(.*)\.md", MarkdownHandler, args)])
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
