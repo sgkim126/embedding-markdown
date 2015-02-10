7b1632afa2007814786d0bda1145d75f34991806
Seulgi Kim <dev@seulgi.kim>
2015-02-10 23:36:12 +0900

    Implement .js embedding.
    It's not safe. but it runs on my needs.

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ def convert_to_html(md):
     return markdown.markdown(md, extensions=['attr_list'])
 
 
+def convert_to_javascript(md):
+    document_format = "document.write('%s')"
+    html = convert_to_html(md)
+    return document_format % html.replace("'", "\\'").replace("\n", "")
+
+
 class MarkdownHandler(tornado.web.RequestHandler):
     def initialize(self, target):
         self.target = target
@@ -45,11 +51,27 @@ class HTMLHandler(tornado.web.RequestHandler):
         self.finish()
 
 
+class JavascriptHandler(tornado.web.RequestHandler):
+    def initialize(self, target):
+        self.target = target
+
+    @tornado.web.asynchronous
+    def get(self, path):
+        get_md_async(self.target, path, self._on_download)
+
+    def _on_download(self, response):
+        self.write(convert_to_javascript(response.body.decode("utf-8")))
+        self.set_header('Content-Type',
+                        'application/javascript; charset="utf-8"')
+        self.finish()
+
+
 if __name__ == '__main__':
     assert (len(sys.argv) == 2)
     args = dict(target=sys.argv[1])
     Application = tornado.web.Application
     application = Application([(r"/(.*)\.html", HTMLHandler, args),
+                               (r"/(.*)\.js", JavascriptHandler, args),
                                (r"/(.*)\.md", MarkdownHandler, args)])
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
